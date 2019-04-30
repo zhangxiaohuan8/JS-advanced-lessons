0e3bcf212d882a66ad6587135e52be786f3c0218
zhangxiaohuan8 <2916244782@qq.com>
2019-04-30 20:28:00 +0800

    2019-4-30

---
+++ 课程练习/LS17/js/demo07.js
@@ -0,0 +1,14 @@
+/**
+ * Created by qile on 2017/8/14.
+ */
+//通过Object.create静态方法创建的对象的原型共享问题
+var superObj = {
+    x:1,
+    y:2
+};
+var subObj_First = Object.create(superObj);
+var subObj_Second = Object.create(superObj);
+// subObj_First.__proto__.x = 5;
+subObj_First.x = 5;
+//若此行写为subObj_First.x = 5;结果变为1
+console.log(subObj_Second.x);//5
